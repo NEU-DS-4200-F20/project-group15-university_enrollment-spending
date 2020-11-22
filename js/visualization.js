d09d2b14b5fa1baea0cd2252b63b5e3b71ad4a8e
marcleonti <65128561+marcleonti@users.noreply.github.com>
2020-11-22 15:35:02 -0500

    Update visualization.js
    minor formatting edits for code readability

--- js/visualization.js
+++ js/visualization.js
@@ -1,56 +1,42 @@
 // // Immediately Invoked Function Expression to limit access to our
-// // variables and prevent
+// // variables and prevent conflicts
 
 (() => {
   d3.csv("data/mergedDataFinal2.csv").then((data) => {
     // General event type for selections, used by d3-dispatch
     // https://github.com/d3/d3-dispatch
     const dispatchString = "selectionUpdated";
-  
-  
+
     //console.log(data, "This is all the data") //--> Uncomment to see what data looks like
-  
-  
+
     // Create a line chart given x and y attributes, labels, offsets;
     // a dispatcher (d3-dispatch) for selection events;
     // a div id selector to put our svg in; and the data to use.
-  
+
     //define five fields to draw line chart
     const fields = [
-      {
-        slabel: "Instruction", //percent field
-         dlabel: "Instruction - Current year total", // number of 
-      },
-      {
-        slabel: "Academic_Support",
-        dlabel: "Academic support - Current year total",
-      },
-      {
-        slabel: "Student_Services",
-        dlabel: "Student services - Current year total",
-      },
-      {
-        slabel: "Institutional_Support",
-        dlabel: "Institutional support - Current year total",
-      },
-      {
-        slabel: "Auxillary_Enterprises",
-        dlabel: "Auxiliary enterprises -- Current year total",
-      },
+      {slabel: "Instruction",
+         dlabel: "Instruction - Current year total",},
+      {slabel: "Academic_Support",
+        dlabel: "Academic support - Current year total",},
+      {slabel: "Student_Services",
+        dlabel: "Student services - Current year total",},
+      {slabel: "Institutional_Support",
+        dlabel: "Institutional support - Current year total",},
+      {slabel: "Auxillary_Enterprises",
+        dlabel: "Auxiliary enterprises -- Current year total",},
     ];
-  
+
       //Assiginig a color to each university
       //https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
-  
     const legends = [...new Set(data.map((e) => e.SchoolName))].map(
       (sn, i) => ({
         name: sn,
         color: colors[i],
        })
     );
-      
     //console.log(legends, "These are the colors") //--> Uncomment to see how it looks like
-  
+
     //get linecharts holder element for adding each linechart dynamically
     const linechartsHolder = d3.select(".linecharts-holder");
     //adding 5 divs for each line chart and they will have 5 different schools
@@ -60,12 +46,11 @@
       .enter()
       .append("div")
       .attr("class", (d) => `line-chart-container line-chart-${d.slabel}`);
-  
-       
+
       updateLineCharts();
-      
-      //Same as in assignment 8, slighly different
-      
+
+      //Same concept as Assignment 8, slightly modified
+
     const violin = violinplotchart()
       .xFields(fields.map((d) => d.slabel))
       .xLabel("Columns")
@@ -75,14 +60,12 @@
       data,
       legends
     );
-  
+
     // When the violin selection is updated via brushing,
     // tell the linechart to update it's selection (linking)
-      
     violin.selectionDispatcher().on(dispatchString, function (selectedData) {
-      //
       const one = selectedData[0];
-      //Check if there is matched records
+      //Check if there are matched records
       if (one) {
         const years = [...new Set(selectedData.map((d) => d.year))];
         const reDrawData = data.filter((d) => years.includes(d["YearData"]));
@@ -103,7 +86,7 @@
         updateLineCharts();
       }
     });
-  
+
     //add line charts for five fields
     function updateLineCharts() {
       fields.forEach((field, i) => {
