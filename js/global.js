9fb68ff644d487045102ff5309e02081e97fb39b
marcleonti <65128561+marcleonti@users.noreply.github.com>
2020-11-22 15:30:39 -0500

    Update global.js
    Changed colors to websafe, added comments

--- js/global.js
+++ js/global.js
@@ -1,12 +1,12 @@
-//Defining the color set that will be used for both linegraph and violinplot
+// Define the color set that will be used for both linegraph and violinplot
 const colors = [
-  "#e41a1c",
-  "#377eb8",
-  "#4daf4a",
-  "#984ea3",
-  "#ff7f00",
-  "#ffff33",
-  "#a65628",
-  "#f781bf",
-  "#999999",
+  "#ff0000", // Bridgewater (red)
+  "#0000ff", // Fitchburg (blue)
+  "#009900", // Framingham (green)
+  "#9900cc", // Salem (violet)
+  "#ff8800", // Westfield (orange)
+  "#ffee00", // Worcester (yellow)
+  "#993300", // ? (brown)
+  "#ff00ff", // ? (pink)
+  "#999999", // ? (gray)
 ];
