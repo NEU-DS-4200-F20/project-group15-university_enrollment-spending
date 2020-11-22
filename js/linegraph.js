f73a16faac01d45b4ddbf53f31b064481f120f61
marcleonti <65128561+marcleonti@users.noreply.github.com>
2020-11-22 15:32:20 -0500

    Update linegraph.js
    Linegraph width set to 400px in line 12, Formatting edits for code readability

--- js/linegraph.js
+++ js/linegraph.js
@@ -1,15 +1,12 @@
 // Initialize a line chart. Modeled after Mike Bostock's
 // Reusable Chart framework https://bost.ocks.org/mike/chart/
+
+// Based on Mike Bostock's margin convention
+// https://bl.ocks.org/mbostock/3019563
+
 function linechart() {
-  // Based on Mike Bostock's margin convention
-  // https://bl.ocks.org/mbostock/3019563
-  let margin = {
-      top: 40,
-      left: 80,
-      right: 20,
-      bottom: 40,
-    },
-    width = 500 - margin.left - margin.right,
+  let margin = {top: 40,left: 80,right: 20,bottom: 40,},
+    width = 400 - margin.left - margin.right,
     height = 250 - margin.top - margin.bottom,
     xValue = (d) => d[0],
     yValue = (d) => d[1],
@@ -20,7 +17,7 @@ function linechart() {
     yScale = d3.scaleLinear();
   // selectableElements = d3.select(null),
   // dispatcher;
-  
+
   // Create the chart by adding an svg to the div with the id
   // specified by the selector using the given data
   function chart(selector, data, legends, hasLegends = false) {
@@ -28,15 +25,15 @@ function linechart() {
     if (hasLegends) {
       margin.right = 300;
     }
-  
+
     //clear all svg elements before start to draw new chart
-    d3.select(selector).selectAll("*").remove();    
+    d3.select(selector).selectAll("*").remove();
     let svg = d3
       .select(selector)
       .append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom);
-  
+
     // add legends into chart
     if (hasLegends) {
       const legendsG = svg
@@ -53,7 +50,7 @@ function linechart() {
         .append("g")
         .attr("class", "legend-g")
         .attr("transform", (_, i) => `translate(0, ${i * 30})`);
-  
+
       legendG
         .append("circle")
         .style("fill", (d) => d.color)
@@ -65,27 +62,27 @@ function linechart() {
         .attr("dy", ".4em")
         .text((d) => d.name);
     }
-  
+
     svg = svg
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-      
+
     //Define scales
     xScale.domain(d3.group(data, xValue).keys()).rangeRound([0, width]);
-  
+
     yScale
       .domain([
         d3.min(data, (d) => parseFloat(yValue(d))),
         d3.max(data, (d) => parseFloat(yValue(d))),
       ])
       .rangeRound([height, 0]);
-  
+
     // X axis
     let xAxis = svg
       .append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(xScale));
-  
+
     // Put X axis tick labels at an angle
     xAxis
       .selectAll("text")
@@ -93,7 +90,7 @@ function linechart() {
       .attr("dx", "-.8em")
       .attr("dy", ".15em")
       .attr("transform", "rotate(-65)");
-  
+
     // X axis label
     xAxis
       .append("text")
@@ -101,7 +98,7 @@ function linechart() {
       .attr("text-anchor", "end")
       .attr("transform", "translate(" + width + ",-10)")
       .text(xLabelText);
-  
+
     // Y axis and label
     let yAxis = svg
       .append("g")
@@ -114,7 +111,7 @@ function linechart() {
         "translate(" + (yLabelOffsetPx - margin.left) + ", -10)"
       )
       .text(yLabelText);
-  
+
     //grouped path data per each school
     const sumstat = d3.group(data, (d) => d.SchoolName);
     // Add the lines
@@ -123,14 +120,14 @@ function linechart() {
       .data(Array.from(sumstat.values()))
       .enter()
       .append("g");
-  
+
     pathG
       .append("path")
       .attr("fill", "none")
       .attr("stroke", (_, i) => colors[i])
       .attr("stroke-width", 3)
       .attr("d", (d) => d3.line().x(X).y(Y)(d));
-  
+
     // Add the points
     let points = pathG
       .selectAll(".line-point")
@@ -142,68 +139,67 @@ function linechart() {
       .attr("cx", X)
       .attr("cy", Y)
       .attr("r", 5);
-      
+
       return chart;
   }
-  
+
   // The x-accessor from the datum
   function X(d) {
     return xScale(xValue(d));
   }
-  
+
     // The y-accessor from the datum
   function Y(d) {
     return yScale(yValue(d));
   }
-  
+
   chart.margin = function (_) {
     if (!arguments.length) return margin;
     margin = _;
     return chart;
   };
-  
+
   chart.width = function (_) {
     if (!arguments.length) return width;
     width = _;
     return chart;
   };
-  
+
   chart.height = function (_) {
     if (!arguments.length) return height;
     height = _;
     return chart;
   };
-  
+
   chart.x = function (_) {
     if (!arguments.length) return xValue;
     xValue = _;
     return chart;
   };
-  
+
   chart.y = function (_) {
     if (!arguments.length) return yValue;
     yValue = _;
     return chart;
   };
-  
+
   chart.xLabel = function (_) {
     if (!arguments.length) return xLabelText;
     xLabelText = _;
     return chart;
   };
-  
+
   chart.yLabel = function (_) {
     if (!arguments.length) return yLabelText;
     yLabelText = _;
     return chart;
   };
-  
+
   chart.yLabelOffset = function (_) {
     if (!arguments.length) return yLabelOffsetPx;
     yLabelOffsetPx = _;
     return chart;
   };
-  
+
   return chart;
 }
-  
