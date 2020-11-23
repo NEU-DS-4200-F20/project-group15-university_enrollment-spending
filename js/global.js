232e907d03284011b62ea70df4b4fdd8bc406c3a
marcleonti <65128561+marcleonti@users.noreply.github.com>
2020-11-22 21:37:30 -0500

    Update global.js
    Made blue a little lighter

--- js/global.js
+++ js/global.js
@@ -1,7 +1,7 @@
 // Define the color set that will be used for both linegraph and violinplot
 const colors = [
   "#ff0000", // Bridgewater (red)
-  "#0000ff", // Fitchburg (blue)
+  "#0066ff", // Fitchburg (blue)
   "#009900", // Framingham (green)
   "#9900cc", // Salem (violet)
   "#ff8800", // Westfield (orange)
