00a1becaa3c188d00997dec8095bb48613348264
marcleonti <65128561+marcleonti@users.noreply.github.com>
2020-11-30 21:19:53 -0500

    Update ftelinegraph.js
    Moved legend below violin graph, made graph wider, small fix to mouseover details

--- js/ftelinegraph.js
+++ js/ftelinegraph.js
@@ -1,12 +1,12 @@
-// Initialize a line chart. Modeled after Mike Bostock's 
+// Initialize a line chart. Modeled after Mike Bostock's
 // Reusable Chart framework https://bost.ocks.org/mike/chart/
 
 // Based on Mike Bostock's margin convention
 // https://bl.ocks.org/mbostock/3019563
 
 function ftelinechart() {
-    let margin = { top: 40, left: 80, right: 20, bottom: 40 },
-      width = 600 - margin.left - margin.right,
+    let margin = { top: 25, left: 50, right: 20, bottom: 20 },
+      width = 800 //- margin.left - margin.right,
       height = 280 - margin.top - margin.bottom,
       xValue = (d) => d[0],
       yValue = (d) => d[1],
@@ -15,7 +15,7 @@ function ftelinechart() {
       yLabelOffsetPx = 0,
       xScale = d3.scalePoint(),
       yScale = d3.scaleLinear();
-  
+
     // Create the chart by adding an svg to the div with the id
     // specified by the selector using the given data
     function chart(selector, data, legends) {
@@ -24,21 +24,21 @@ function ftelinechart() {
         .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom);
-  
+
       svg = svg
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-  
+
       //Define scales
       xScale.domain(d3.group(data, xValue).keys()).rangeRound([0, width]);
-  
+
       yScale
         .domain([
           d3.min(data, (d) => parseFloat(yValue(d))),
           d3.max(data, (d) => parseFloat(yValue(d))),
         ])
         .rangeRound([height, 0]);
-  
+
       // X axis and label
       let xAxis = svg
         .append("g")
@@ -48,14 +48,14 @@ function ftelinechart() {
         // .style("text-anchor", "end")
         // .attr("dx", "-.8em")
         // .attr("dy", ".15em");
-  
+
       xAxis
         .append("text")
         .attr("class", "axisLabel")
         .attr("text-anchor", "end")
         .attr("transform", "translate(" + width + ",-10)")
         .text(xLabelText);
-  
+
       // Y axis and label
       let yAxis = svg
         .append("g")
@@ -68,17 +68,17 @@ function ftelinechart() {
           "translate(" + (yLabelOffsetPx - margin.left) + ", -10)"
         )
         .text(yLabelText);
-  
+
       // group path data per each school
       const sumstat = d3.group(data, (d) => d.SchoolName);
-  
+
       // Add the lines
       const pathG = svg
         .selectAll(".path-g")
         .data(Array.from(sumstat))
         .enter()
         .append("g");
-  
+
       pathG
         .append("path")
         .attr("fill", "none")
@@ -89,14 +89,14 @@ function ftelinechart() {
         })
         .attr("stroke-width", 3)
         .attr("d", (d) => d3.line().x(X).y(Y)(d[1]));
-  
+
       //define the div for the tooltip
       var div = d3
         .select("body")
         .append("div")
         .attr("class", "tooltip")
         .style("opacity", 0);
-  
+
       /* //define the var of tooltip
       var tooltip = d3.select("#linegraph")
       .append("div")
@@ -107,9 +107,9 @@ function ftelinechart() {
       .style("border-width", "1px")
       .style("border-radius", "5px")
       .style("padding", "5px"); */
-  
+
       function displaydata(d) {}
-  
+
       // Add the points
       let points = pathG
         .selectAll(".line-point")
@@ -121,7 +121,7 @@ function ftelinechart() {
         .attr("cx", X)
         .attr("cy", Y)
         .attr("r", 2)
-  
+
         //mouse events
         .on("mouseover", function (event, d) {
           div
@@ -130,18 +130,18 @@ function ftelinechart() {
             .style("opacity", 0.9)
             .style("left", event.pageX + "px")
             .style("top", event.pageY - 28 + "px");
-          div.html("Data Value is:" + d.AuxiliaryEnterprisesRevenuesPerFTE);
+          div.html(d.SchoolName + "<br/>FTE students:" + d.TotalFTE);
           console.log(d);
-  
+
           /*tooltip
         .style("opacity", 1)
         .html(d3.select(this))
         .style("left", (event.layerX) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
         .style("top", (event.layerY) + "px") */
-  
+
           //use raise() to bring the element forward when hovering the mouse
           //hide when mouse moves away
-  
+
           const selection = d3.select(this).raise();
           selection
             .transition()
@@ -154,12 +154,12 @@ function ftelinechart() {
         /*.on('mousemove', function(event, d) {
       tooltip
       .html("Data Value" + d)
-    
+
       //.style('transform', `translate(${event.layerX - 300}px, ${event.layerY - 300}px)`)
       .style("left", (d3.pointer(this)[0]) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
       .style("top", (d3.pointer(this)[1]) + "px")
   })  */
-  
+
         /*  repeat();
           // ref line: https://bl.ocks.org/d3noob/bf44061b1d443f455b3f857f82721372
           // stop transition: https://stackoverflow.com/questions/26903355/how-to-cancel-scheduled-transition-in-d3
@@ -179,7 +179,7 @@ function ftelinechart() {
               .style("opacity", 1)
               .on("end", repeat);
           } */
-  
+
         .on("mouseout", function (d) {
           const selection = d3.select(this);
           selection
@@ -189,7 +189,7 @@ function ftelinechart() {
             .attr("r", 2)
             .style("opacity", 1);
         });
-  
+
       function handlemousemove(event, d) {
         // svg.append('text').text(function(){return 'shubham'}).style('top',(d3.event.layerY + 10)+'px').style('left',(d3.event.layerX + 10) + 'px')
         console.log(tooltip.style.top, event.layerY);
@@ -204,68 +204,67 @@ function ftelinechart() {
           .style("display", "block")
           .style("color", "red");
       }
-  
+
       return chart;
     }
-  
+
     // The x-accessor from the datum
     function X(d) {
       return xScale(xValue(d));
     }
-  
+
     // The y-accessor from the datum
     function Y(d) {
       return yScale(yValue(d));
     }
-  
+
     chart.margin = function (_) {
       if (!arguments.length) return margin;
       margin = _;
       return chart;
     };
-  
+
     chart.width = function (_) {
       if (!arguments.length) return width;
       width = _;
       return chart;
     };
-  
+
     chart.height = function (_) {
       if (!arguments.length) return height;
       height = _;
       return chart;
     };
-  
+
     chart.x = function (_) {
       if (!arguments.length) return xValue;
       xValue = _;
       return chart;
     };
-  
+
     chart.y = function (_) {
       if (!arguments.length) return yValue;
       yValue = _;
       return chart;
     };
-  
+
     chart.xLabel = function (_) {
       if (!arguments.length) return xLabelText;
       xLabelText = _;
       return chart;
     };
-  
+
     chart.yLabel = function (_) {
       if (!arguments.length) return yLabelText;
       yLabelText = _;
       return chart;
     };
-  
+
     chart.yLabelOffset = function (_) {
       if (!arguments.length) return yLabelOffsetPx;
       yLabelOffsetPx = _;
       return chart;
     };
-  
+
     return chart;
   }
-  
