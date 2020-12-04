7de8d12196f2d52ba3a5e069b1a97a4851f2f753
marcleonti <65128561+marcleonti@users.noreply.github.com>
2020-12-04 00:19:43 -0500

    Update global.js
    made green lighter

--- js/global.js
+++ js/global.js
@@ -2,23 +2,12 @@
 const colors = [
 	'#990099', // violet - Salem State Univ.
 	'#ff0000', // red - Bridgewater State Univ.
-	'#006600', // green - Fitchburg State Univ.
+	'#009900', // green - Fitchburg State Univ.
 	'#0000ff', // blue - Framingham State Univ.
 	'#ff9900', // orange - Westfield State Univ.
 	'#ffff33', // yellow - Worcester State Univ.
 	'#ff00ff', // pink - State U Average
-
 	'#999999', // gray - UMass Average
-	'#00ccff', // gray - MA Average (without SSU)
-
-	'#000000', // black - Central CT State Univ.
-	'#000000', // black - Rhode Island College
-	'#000000', // black - Southern CT State Univ.
-	'#000000', // black - Univ. of Rhode Island
-	'#000000', // black - Western CT State Univ.
-	'#000000', // black - RI & CT Average
+	'#00ccff', // light blue - MA Average (without SSU)
+	'#000000', // black -
 ];
-
-//	'#ff00ff', // pink - UMass Boston
-//	'#00cc00', // light green - UMass Dartmouth
-//	'#00ccff', // light blue - UMass Lowell
