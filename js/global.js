c4fce18a95d486467c86fbdc22ccae863d618294
marcleonti <65128561+marcleonti@users.noreply.github.com>
2020-12-04 13:53:53 -0500

    Update global.js
    Added new color schemes just in case

--- js/global.js
+++ js/global.js
@@ -1,13 +1,92 @@
 // Define the color set that will be used for both linegraph and violinplot
-const colors = [
-	'#990099', // violet - Salem State Univ.
-	'#ff0000', // red - Bridgewater State Univ.
-	'#009900', // green - Fitchburg State Univ.
-	'#0000ff', // blue - Framingham State Univ.
-	'#ff9900', // orange - Westfield State Univ.
+// 9-class set obtained from ColorBrewer 2.0
+//https://colorbrewer2.org/#type=qualitative&scheme=Set1&n=9
+
+const colors =
+[
+	'#984ea3', // violet - Salem State Univ.
+	'#e41a1c', // red - Bridgewater State Univ.
+	'#4daf4a', // green - Fitchburg State Univ.
+	'#377eb8', // blue - Framingham State Univ.
+	'#ff7f00', // orange - Westfield State Univ.
 	'#ffff33', // yellow - Worcester State Univ.
-	'#ff00ff', // pink - State U Average
+	'#f781bf', // pink - State U Average
 	'#999999', // gray - UMass Average
-	'#00ccff', // light blue - MA Average (without SSU)
+	'#a65628', // brown - MA Average (without SSU)
 	'#000000', // black -
 ];
+
+// full 12-class paired set from ColorBrewer 2.0
+// [
+// 	'#a6cee3', // Lt Blue
+// 	'#1f78b4', // Dk Blue
+// 	'#b2df8a', // Lt Green
+// 	'#33a02c', // Dk Green
+// 	'#fb9a99', // Pink
+// 	'#e31a1c', // Red
+// 	'#fdbf6f', // Tan
+// 	'#ff7f00', // Orange
+// 	'#cab2d6', // Lt Violet
+// 	'#6a3d9a', // Dk Violet
+// 	'#ffff99', // Yellow
+// 	'#b15928', // Brown
+// ]
+
+// websafe colors I originally selected
+// [
+// 	'#990099', // violet
+// 	'#e41a1c', // red
+// 	'#009900', // green
+// 	'#0000ff', // blue
+// 	'#ff9900', // orange
+// 	'#ffff33', // yellow
+// 	'#ff00ff', // pink
+// 	'#999999', // gray
+// 	'#00ccff', // light blue
+// 	'#000000', // black
+// ]
+
+// "Bold" 12 color qualitative scheme from https://carto.com/carto-colors/
+// [
+// #E58606,
+// #5D69B1,
+// #52BCA3,
+// #99C945,
+// #CC61B0,
+// #24796C,
+// #DAA51B,
+// #2F8AC4,
+// #764E9F,
+// #ED645A,
+// #CC3A8E,
+// #A5AA99,
+// ]
+
+// "Vivid" 12 color qualitative scheme from https://carto.com/carto-colors/
+// [
+// 	#E58606,
+// 	#5D69B1,
+// 	#52BCA3,
+// 	#99C945,
+// 	#CC61B0,
+// 	#24796C,
+// 	#DAA51B,
+// 	#2F8AC4,
+// 	#764E9F,
+// 	#ED645A,
+// 	#CC3A8E,
+// 	#A5AA99,
+// ]
+
+// 9-color palette from https://medialab.github.io/iwanthue/
+// [
+// 	"#4b393a", // almost black
+// 	"#8ccf53", // leaf
+// 	"#6a47b4", // navy
+// 	"#bd9e4f", // baby poop
+// 	"#c348a4", // violet
+// 	"#7ab693", // seafoam
+// 	"#c24d3e", // brick
+// 	"#7297c2", // sky
+// 	"#c78ca5", // rose
+// ]
