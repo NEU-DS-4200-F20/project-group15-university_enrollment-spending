d73bc8ea68729ef49774c5fd43cf667f3eae0e60
marcleonti <65128561+marcleonti@users.noreply.github.com>
2020-12-09 14:56:13 -0500

    Fixed sizing to be dynamic to match the violin width

--- js/legend.js
+++ js/legend.js
@@ -1,7 +1,7 @@
 function legend() {
-	const margin = {top: 10, right: 10, bottom: 10, left: 60},
-	width = 730,
-	height = 90;
+	const margin = {top: 20, left: 20},
+	width = violinWidth,
+	height = 130;
 
 	let selectedSchools = [];
 
@@ -9,12 +9,12 @@ function legend() {
 		let svg = d3
 		.select(selector)
 		.append('svg')
-		.attr('width', width + margin.left + margin.right)
-		.attr('height', height + margin.top + margin.bottom);
+		.attr('width', width - margin.left)
+		.attr('height', height);
 
 		svg = svg
 		.append('g')
-		.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+		.attr('transform', `translate(${margin.left},${margin.top})`);
 
 		// add legends into chart if applicable
 		let yPos = 0,
@@ -27,11 +27,12 @@ function legend() {
 		.append('g')
 		.attr('class', 'legend-for-schoolname')
 		.attr('transform', (_, i) => {
-			if (i % 3 === 0) {
-				yPos += 25;
-				xPos = 0;
+			if (i % 3 === 0) { // 3 is the number of columns
+				yPos += 25; // legend row height
+				xPos = 0; // no horizontal cushion needed
 			} else {
-				xPos = (i % 3) * 250;
+				xPos = (i % 3) * (violinWidth - margin.left)/3;
+				// Add horizontal cushion
 			}
 			return `translate(${xPos}, ${yPos})`;
 		});
@@ -43,16 +44,24 @@ function legend() {
 		.attr('r', 7)
 
 		.on('click', function (event, d) {
-			// If the selections is already in the filter list, remove it
+			// If the selection is already in the filter list, remove it
 			if (selectedSchools.includes(d.name)) {
 				selectedSchools = selectedSchools.filter((s) => s !== d.name);
-				d3.select(this).style('stroke', 'black').style('stroke-width', 1);
+				d3.select(this)
+				.attr('r', 7)
+				.style('stroke', 'black')
+				.style('stroke-width', 1)
+				;
 			}
 
 			// otherwise, add the selection to the filter list
 			else {
 				selectedSchools = [...selectedSchools, d.name];
-				d3.select(this).style('stroke', 'pink').style('stroke-width', 3);
+				d3.select(this)
+				.attr('r', 10)
+				.style('stroke', 'pink')
+				.style('stroke-width', 5)
+				;
 			}
 
 			// Get the name of our dispatcher's event
